abaa29265ec3eab52667004223f960f190e0dc0d
JoYoneyama <jvsyoneyama@gmail.com>
2024-10-23 16:43:42 -0300

    Detalhes finais

--- src/components/OutlineButtons.jsx
+++ src/components/OutlineButtons.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 export function OutlineButtons({ description, onClick }) {
   return (
     <>
-      <Button variant="outline-secondary" onClick={onClick}>{description}</Button>{' '}
+      <Button variant="outline-light" onClick={onClick}>{description}</Button>{' '}
     </>
   );
 }
